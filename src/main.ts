3aa862b0443c2f7bdc8e71c76a3b8356d0fa44db
monman53 <tetsuro53@gmail.com>
2024-08-09 15:48:21 +0900

    Default pause

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ export const app = ref({
   width: window.innerWidth,
   height: window.innerHeight,
   n: 10,
-  pause: false,
+  pause: true,
   t: 2, // > 1 for initial Bezier creation
   c: vec(0, 0),
   prevC: vec(0, 0),
